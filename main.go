ca8cbe1eddb5d476853389c308b768f9046ee839
Gabriel P. Bezerra <gabriel.bezerra@ufv.br>
2021-06-02 14:04:42 -0300

    Registra os endpoints da API para registro e obtencao de posicao

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"log"
 
+	"github.com/gabrielpbzr/findme/core"
 	"github.com/gabrielpbzr/findme/infra"
+	"github.com/gabrielpbzr/findme/web"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,15 +13,16 @@ func main() {
 	dbHandler := infra.Database{DSN: "data/data.db"}
 
 	db, err := dbHandler.Open()
+	positionService := core.NewPositionService(db)
+
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	defer dbHandler.Close(db)
 	router := gin.Default()
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.String(200, "HELLO GIN-GONIC!")
-	})
+
+	web.MakeHandlers(router, positionService)
 
 	router.Run()
 }
